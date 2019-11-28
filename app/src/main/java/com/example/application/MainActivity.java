59436c5dac0a1c21ffe458e6ba3368a44d3c8871
Komal2006 <komaldeepkr99@gmail.com>
2019-11-28 17:50:45 -0500

    Initial commit

---
+++ app/src/main/java/com/example/application/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mIntent= new Intent(MainActivity.this,Main2Activity.class);
+                startActivity(mIntent);
+                finish();
+            }
+        },6000);
+        }
+    }
+
